4329770c2eb587766b03ed03ff5663e353b324cd
chinkarari <ec2-user@ip-172-31-51-215.ec2.internal>
2018-07-06 00:00:14 +0000

    CRUD tasks

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>課題新規作成</h1>
+    
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+    
+        {!! Form::label('content', '課題: ') !!}
+        {!! Form::text('content') !!}
+        
+        {!! Form::submit('投稿') !!}
+        
+    {!! Form::close() !!}
+
+@endsection
---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>id = {{ $task->id }} の課題</h1>
+    
+    <p>{{ $task->content }}</p>
+    
+    {!! link_to_route('tasks.edit', '課題を編集', ['id' => $task->id]) !!}
+    
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除') !!}
+    {!! Form::close() !!}
+@endsection
